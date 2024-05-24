2aa0e7785462e0e4486bfae51302392261f40563
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-05-24 00:10:51 -0400

    app complete

--- server.js
+++ server.js
@@ -7,3 +7,27 @@ const uri = process.env.ATLAS_URI
 const client = new MongoClient(uri)
 let PORT = process.env.PORT || 3500
 let connectedClient, db 
+async function connectToMongoDB(){
+    try {
+        connectedClient = await client.connect()
+        console.log('Server connected to MongoDB!')
+    } catch(error) {
+        console.log(error)
+    } finally {
+        db = connectedClient.db('DBconnectionTest')
+        app.listen(PORT, ()=>{
+            console.log(`Server is now listening on port: ${PORT}`)
+        })
+    }
+}
+connectToMongoDB()
+app.set('view engine','ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.get('/', (req,res)=>{
+    db.collection('test').find().toArray()
+    .then(data=>{
+        res.render('index.ejs',{info:data})
+    })
+    // res.send(`Mongo DB Atlas now connected to server listening on port: ${PORT}`)
+})
